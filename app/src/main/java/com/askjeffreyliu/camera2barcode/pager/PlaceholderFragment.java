40e45c147dfe4b7607d89c82d31c947b42d6e4f4
Jeffrey Liu <jeffreyliu@admins-13-MacBook-Pro.local>
2017-03-25 09:35:48 -0700

    first commit

---
+++ app/src/main/java/com/askjeffreyliu/camera2barcode/pager/PlaceholderFragment.java
@@ -0,0 +1,53 @@
+package com.askjeffreyliu.camera2barcode.pager;
+
+/**
+ * Created by jeffreyliu on 3/25/17.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.askjeffreyliu.camera2barcode.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private static final String ARG_SECTION_TITLE = "section_title";
+
+    public PlaceholderFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static PlaceholderFragment newInstance(int sectionNumber, String title) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        args.putString(ARG_SECTION_TITLE, title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_test, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+        textView.setText(getArguments().getString(ARG_SECTION_TITLE));
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/askjeffreyliu/camera2barcode/pager/SectionsPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.askjeffreyliu.camera2barcode.pager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by jeffreyliu on 3/25/17.
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position, getPageTitle(position).toString());
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "QR code";
+            case 1:
+                return "Data Matrix";
+            case 2:
+                return "PDF-417";
+        }
+        return null;
+    }
+}
